5c71e2cfa76b14667cd525f7ac74478f8adaba21
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-13 19:14:15 +0000

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script prints a welcome message by concatenating two strings
 str1 = "Welcome to"
 str2 = "Holberton School!"
-print(str1 + " " + str2)
+print(f"{str1} {str2}")
+

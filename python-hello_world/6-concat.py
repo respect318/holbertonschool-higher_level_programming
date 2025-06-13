e7162fca53a0400e912e17e6cddf8870e596aa1c
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-13 19:17:31 +0000

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = "{} {}".format(str1, str2)  # Added space after comma
+str3 = "{} {}".format(str1,str2)
 print("Welcome to {}!".format(str3))

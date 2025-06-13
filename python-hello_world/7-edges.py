d1234170eeea252795b3e439a12255fcb5c9b15f
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-13 18:56:00 +0000

    7-edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# This script slices a word to produce specific parts
+# This script slices the word into parts
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("First 3 letters:", word_first_3)
-print("Last 2 letters:", word_last_2)
-print("Middle word:", middle_word)
+print(word_first_3)
+print(word_last_2)
+print(middle_word)

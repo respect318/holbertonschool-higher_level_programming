efb1fdd7e16896d3d7b155d8a65cf9d5423a3542
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-18 17:04:21 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
-def fizzBuzz(n):
-    res = [] 
-
-    for i in range(1, n + 1):
-      
-        # Initialize an empty string for the current result
-        s = "" 
-
-        # Divides by 3, add Fizz
-        if i % 3 == 0:
-            s += "Fizz"
-            
-        # Divides by 5, add Buzz
-        if i % 5 == 0:
-            s += "Buzz"
-            
-        # Not divisible by 3 or 5, add the number
-        if not s:
-            s += str(i)
-		
-        # Append the current result to the list
-        res.append(s) 
-
-    return res
-
-if __name__ == "__main__":
-    n = 20 
-    res = fizzBuzz(n) 
-
-    for s in res:
-        print(s, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")

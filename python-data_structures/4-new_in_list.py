5cf73a328712ab8b2e1090e8abfacfd5b310504f
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-20 07:49:27 +0000

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None
+    if my_list is None:
         return None
     copy_list = my_list[:]
-    if idx <0  or idx >= len(copy_list):
+    if idx < 0 or idx >= len(copy_list):
         return copy_list
     copy_list[idx] = element
     return copy_list

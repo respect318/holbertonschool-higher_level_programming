3ae8d57bfe8a3dfd2e653d3432bfabf45fca5a17
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-22 07:27:12 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[element ** 2 for element in row] for row in matrix]
+    """Returns a new matrix with squares of all values in the input matrix."""
+    return [[x ** 2 for x in row] for row in matrix]
 

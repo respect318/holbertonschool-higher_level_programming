915afa4e770b107053277ece7b3957cc8bd863ba
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-26 08:25:57 +0000

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle with string representation"""
+"""Defines a class Rectangle"""
 
 
 class Rectangle:
     """Represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialize rectangle with optional width and height"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Retrieve the width"""
+        """Gets the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width with validation"""
+        """Sets the width with validation"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieve the height"""
+        """Gets the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height with validation"""
+        """Sets the height with validation"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,17 +38,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the rectangle"""
-        return self.__width * self.__height
+        """Returns the area of the rectangle"""
+        return self.width * self.height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
+        """Returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Return the string representation of the rectangle using '#' characters"""
-        if self.__width == 0 or self.__height == 0:
+        """Returns a string representation with '#'"""
+        if self.width == 0 or self.height == 0:
             return ""
-        rect_lines = []
+        return "\n".join(["#" * self.width for _ in range(self.height)])

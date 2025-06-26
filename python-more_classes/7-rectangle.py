6b2ec38be2e3dc135fdbc9a7cea8482a6c3a835a
respect318 <ferid.mirzezade.education@gmail.com>
2025-06-26 08:33:26 +0000

    7-rectangle.py

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"  # New class attribute for print
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Gets the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return

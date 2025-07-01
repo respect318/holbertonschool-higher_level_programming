2e6e15f4f875d28a5cc356233d869ae837e21aaa
respect318 <ferid.mirzezade.education@gmail.com>
2025-07-01 05:13:40 +0000

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""Check if an object is instance of a subclass of a class."""
+"""
+This module defines a function that checks if
+an object is an instance of a subclass of a given class.
+"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a subclass of a_class."""
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """
+    Return True if obj is an instance of a subclass of a_class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a subclass of a_class,
+        otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class

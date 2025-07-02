8bb002820e0edf217f5933192629d1190329c2fe
respect318 <ferid.mirzezazde.education@gmail.com>
2025-07-02 05:20:37 +0000

    1-my

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-"""Defines the MyList class that inherits from list."""
+"""Defines a class MyList that inherits from list."""
 
 
 class MyList(list):
-    """Custom list class with print_sorted and overridden methods."""
-
-    def __init__(self):
-        """Initialize the MyList object."""
-        list.__init__(self)
-
-    def append(self, item):
-        """Append an item to the list."""
-        list.append(self, item)
-
-    def __str__(self):
-        """Return the string representation of the list."""
-        return list.__str__(self)
+    """A subclass of list with additional functionality."""
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order."""
+        """Print the list in ascending sorted order."""
         print(sorted(self))

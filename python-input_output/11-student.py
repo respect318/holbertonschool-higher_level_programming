b248159c35d5c1b18301edfc5cc611cdc3d38051
respect318 <ferid.mirzezade.education@gmail.com>
2025-07-05 21:03:55 +0400

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module for Student to JSON task
+"""
+
+
+class Student:
+    """This class about Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This function retrieves a dictionary
+         representation of a Student instance"""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """This function that replaces all
+         attributes of the Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)

c58f08dcdce40f587dd2ed345b9d7a25bfd23801
respect318 <ferid.mirzezade.education@gmail.com>
2025-07-16 17:07:56 +0400

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
+url = 'https://intranet.hbtn.io/status'
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
+except Exception as e:
+    print(f"Error fetching the URL: {e}")

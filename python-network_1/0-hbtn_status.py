98695d511ef357c07771292e253cb583f82515ef
respect318 <ferid.mirzezade.education@gmail.com>
2025-07-16 16:58:13 +0400

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""This script fetches a URL using urllib and displays specific response details"""
+"""Fetches https://intranet.hbtn.io/status"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode("utf-8"))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
